f8bd1412cf82e4e901c4f1064aa4646530d2003e
Mikita Havryliuk <chinter.games1@gmail.com>
2023-12-18 10:38:13 +0200

    fixes: remove service limit

--- src/api/services/geo.service.ts
+++ src/api/services/geo.service.ts
@@ -5,8 +5,6 @@ const BASE_GEO_URL = '/geo/1.0/direct'
 
 export const getCities = (query: string) => {
   return weatherClient.get(
-    `${BASE_GEO_URL}?q=${query}&limit=5&appid=${
-      import.meta.env.VITE_WEATHER_API_KEY
-    }`,
+    `${BASE_GEO_URL}?q=${query}&appid=${import.meta.env.VITE_WEATHER_API_KEY}`,
   )
 }
